17016ad07850044904c525708740c373fcc49bbc
Ganeshkumar Venkatesan <ganeshkumar.venkatesan@bhn.com>
2025-01-05 00:17:18 +0530

    initialising themeleaf template

--- src/main/java/com/fwtfig/app/MainVerticle.java
+++ src/main/java/com/fwtfig/app/MainVerticle.java
@@ -3,6 +3,9 @@ package com.fwtfig.app;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.templ.thymeleaf.ThymeleafTemplateEngine;
 
 public class MainVerticle extends AbstractVerticle {
   public static void main(String[] args) {
@@ -12,17 +15,36 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888).onComplete(http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+    ThymeleafTemplateEngine engine = ThymeleafTemplateEngine.create(vertx);
+    Router router = Router.router(vertx);
+
+    router.get().handler(ctx -> {
+      // we define a hardcoded title for our application
+      JsonObject data = new JsonObject();
+//        .put("welcome", "Hi there!");
+
+      // and now delegate to the engine to render it.
+      engine.render(data,"templates/index.html", res -> {
+        if (res.succeeded()) {
+          ctx.response().end(res.result());
+        } else {
+          ctx.fail(res.cause());
+        }
+      });
     });
+    vertx.createHttpServer().requestHandler(router).listen(8080);
+
+//    vertx.createHttpServer().requestHandler(req -> {
+//      req.response()
+//        .putHeader("content-type", "text/plain")
+//        .end("Hello from Vert.x!");
+//    }).listen(8888).onComplete(http -> {
+//      if (http.succeeded()) {
+//        startPromise.complete();
+//        System.out.println("HTTP server started on port 8888");
+//      } else {
+//        startPromise.fail(http.cause());
+//      }
+//    });
   }
 }
